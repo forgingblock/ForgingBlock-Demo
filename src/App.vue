3a7c784e6b2343dca413b5c77425c44c02b7368b
Nuno Lopes <nuno.lopes@lopezi.com>
2019-08-27 12:04:27 +0100

    Update key currencies and responsive grid

--- src/App.vue
+++ src/App.vue
@@ -2,14 +2,14 @@
     <div id="app" style="margin-top:40px;">
         <mu-container class="demo-container">
             <mu-row gutter>
-                <mu-col span="3">
+                <mu-col sm="12" md="4">
                     <navigation></navigation>
                     <div id="nav">
                         <router-link to="/">Home</router-link> |
                         <router-link to="/about">About</router-link>
                     </div>
                 </mu-col>
-                <mu-col span="9">
+                <mu-col sm="12" md="8">
                     <router-view></router-view>
                 </mu-col>
             </mu-row>
