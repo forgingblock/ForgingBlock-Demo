353bbd47d18f24c898ccb6b1d89d0ad529b4438e
Nuno Lopes <nuno.lopes@lopezi.com>
2019-08-28 14:54:19 +0100

    Add info to logout interface

--- src/views/Logout.vue
+++ src/views/Logout.vue
@@ -1,5 +1,11 @@
 <template>
-    <h1>Logout</h1>
+    <mu-paper style="padding:1rem;" class="demo-paper" :z-depth="1">
+        <h1>Logout</h1>
+        <p>You are now logged out.</p>
+        <mu-chip class="demo-chip">
+            Login again
+        </mu-chip>
+    </mu-paper>
 </template>
 
 <script>
