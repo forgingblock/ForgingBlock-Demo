5279643e1e639afc897a4acfcb4ee53282b9ae1b
Nuno Lopes <nuno.lopes@lopezi.com>
2019-08-28 14:34:35 +0100

    Crowdfunding info text

--- src/views/Crowdfunding.vue
+++ src/views/Crowdfunding.vue
@@ -1,5 +1,13 @@
 <template>
-    <h1>Crowdfunding</h1>
+    <mu-container>
+        <mu-paper style="padding:1rem;" class="demo-paper" :z-depth="1">
+            <h1>Crowdfunding</h1>
+            <p>Crowdfunding is the practice of funding a project or venture by raising small amounts of money from a large number of people, typically via the Internet. Crowdfunding is a form of crowdsourcing and alternative finance.</p>
+            <mu-chip class="demo-chip">
+                This is work in progress
+            </mu-chip>
+        </mu-paper>
+    </mu-container>
 </template>
 
 <script>
