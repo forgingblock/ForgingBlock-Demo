7e486268fe1df5c99fcb2f27f395369c1859ac82
Nuno Lopes <nuno.lopes@lopezi.com>
2019-09-05 14:22:51 +0100

    Add signin UI modal

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,30 @@
 <template>
-    <div id="app" style="margin-top:40px;">
+    <div id="app" style="margin-top:20px;">
         <mu-container class="demo-container">
+            <mu-row gutter style="margin-bottom:20px;">
+                <mu-col sm="12" md="12">
+                    <mu-flex class="flex-wrapper" justify-content="end">
+                        <mu-flex class="flex-demo" justify-content="end" >
+                            <mu-button @click="openAlertDialog" flat color="primary">LOGIN</mu-button>
+                            <mu-dialog title="Login to Forging Block" width="600" max-width="80%" :esc-press-close="true" :overlay-close="false" :open.sync="openAlert">
+                                <mu-button slot="actions" flat color="primary" @click="closeAlertDialog">Close</mu-button>
+                                <mu-form ref="form" :model="validateForm" class="mu-demo-form">
+                                    <mu-form-item label="username" prop="username" :rules="usernameRules">
+                                        <mu-text-field v-model="validateForm.username" prop="username"></mu-text-field>
+                                    </mu-form-item>
+                                    <mu-form-item label="password" prop="password" :rules="passwordRules">
+                                        <mu-text-field type="password" v-model="validateForm.password" prop="password"></mu-text-field>
+                                    </mu-form-item>
+                                    <mu-form-item>
+                                        <mu-button color="primary" full-width style="margin:0;" @click="submit">Login</mu-button>
+                                    </mu-form-item>
+                                </mu-form>
+                            </mu-dialog>
+                        </mu-flex>
+                    </mu-flex>
+
+                </mu-col>
+            </mu-row>
             <mu-row gutter>
                 <mu-col sm="12" md="3">
                     <navigation></navigation>
@@ -24,6 +48,44 @@
         components: {
             'navigation': navigation,
         },
+        data () {
+            return {
+                openAlert: false,
+                usernameRules: [
+                    { validate: (val) => !!val, message: 'Username must be filled in'},
+                    { validate: (val) => val.length >= 3, message: 'Username length greater than 3'}
+                ],
+                passwordRules: [
+                    { validate: (val) => !!val, message: 'Password must be filled in'},
+                    { validate: (val) => val.length >= 3 && val.length <= 10, message: 'Password length must be greater than 3 and less than 10'}
+                ],
+                validateForm: {
+                    username: '',
+                    password: ''
+                }
+            };
+        },
+        methods: {
+            openAlertDialog () {
+                this.openAlert = true;
+            },
+            closeAlertDialog () {
+                this.openAlert = false;
+            },
+            submit () {
+                this.$refs.form.validate().then((result) => {
+                    console.log('form valid: ', result)
+                });
+            },/*
+            clear () {
+                this.$refs.form.clear();
+                this.validateForm = {
+                    username: '',
+                    password: '',
+                    isAgree: false
+                };
+            }*/
+        }
     };
 
 </script>
